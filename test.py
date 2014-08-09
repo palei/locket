b3e260b26ff272e1d5e0e10dc8ae4c39568a0873
Ilkka Pale <ilkka@pale.fi>
2014-08-09 15:43:05 +0200

    more unit tests

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-from locket import AESCipher
-import unittest
+from locket import AESCipher, save_file, read_file
+import unittest, os
 
-class AESCipherTest(unittest.TestCase):
+class LocketTest(unittest.TestCase):
     
     def setUp(self):
         self.cipher = AESCipher()
@@ -9,10 +9,24 @@ class AESCipherTest(unittest.TestCase):
 
     def test_encrypt_and_decrypt(self):
         text = 'This is some random text.'
-
         data = self.cipher.encrypt(text)
 
-        self.assertEquals(text, self.cipher.decrypt(data), 'The decrypted data should be equal to the text.')
+        assert self.cipher.decrypt(data) == text, 'Decrypted data is not equal to text'
+
+    def test_one_is_not_two(self):
+        assert 1 != 2, 'One should not equal two'
+
+    def test_save_file(self):
+        filename, text = 'test.txt', 'This message will self-destruct in 3 seconds.'
+        data = self.cipher.encrypt(text)
+        save_file(data, filename)
+
+        assert os.path.exists('test.txt') == True, 'Saved file should exist.'
+
+        data = read_file(filename)
+
+        assert self.cipher.decrypt(data) == text, 'Data read and encrypted from file should equal text'
+
 
 if __name__ == '__main__':
     unittest.main()
